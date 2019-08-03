bbbf30618742bb97ffc8f9b1753f27a91ac33787
upDowLeftRight <haveingfuncoding@gmail.com>
2019-08-03 03:44:35 +0100

    biginnings of simplex terain created

--- gjproj/gjproj/main.cpp
+++ gjproj/gjproj/main.cpp
@@ -1,12 +1,27 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include "open-simplex-noise.h"
 
-struct Chunk {
+#define simplexScale 110
+#define amplitude 0.4f
+
+class Chunk {
 public:
 	static const int width = 16;
 	static const int height = 256;
 	int data[width * height];
+	void generate(osn_context* osCont, int chunk) {
+		memset(data, 0, sizeof(int) * width * height);
+		for (int x = 0; x < width; x++) {
+			int h = (open_simplex_noise2(osCont, (double)(x+(chunk*width)) / simplexScale, 0)*amplitude+1)/2*height;
+			printf("%i\n", h);
+			for (int y = 0; y < h; y++) {
+				data[y * width + x] = 1;
+			}
+		}
+	}
 };
+
 
 
 class ourGame : public olc::PixelGameEngine {
@@ -16,14 +31,21 @@ public:
 	std::vector<olc::Sprite*> tiles;
 	int map[9] = { 0,1,2,3,2,2,3,0,1 };
 	float aspectRatio;
+	osn_context* oSimpContext;
+	std::vector<Chunk*> chunks;
 	bool OnUserCreate() {
+		open_simplex_noise(rand(), &oSimpContext);
 		aspectRatio = ScreenWidth() / (float)ScreenHeight();
 		tiles = std::vector<olc::Sprite*>();
-		tiles.push_back(new olc::Sprite("assets/1.png"));
-		tiles.push_back(new olc::Sprite("assets/2.png"));
+		tiles.push_back(new olc::Sprite("assets/b.png"));
+		tiles.push_back(new olc::Sprite("assets/w.png"));
 		tiles.push_back(new olc::Sprite("assets/3.png"));
 		tiles.push_back(new olc::Sprite("assets/4.png"));
-
+		for (int i = 0; i < 20; i++) {
+			Chunk* c = new Chunk();
+			c->generate(oSimpContext, i);
+			chunks.push_back(c);
+		}
 		return true;
 	}
 
@@ -41,15 +63,17 @@ public:
 					olc::Sprite* tile = tiles.at(tileInd);
 					int spritePX = (tilePosX - (int)tilePosX) * tile->width;
 					int spritePY = (tilePosY - (int)tilePosY) * tile->height;
-					Draw(x, y, tile->GetPixel(spritePX, spritePY));
+					Draw(x, ScreenHeight()-y, tile->GetPixel(spritePX, spritePY));
 				}
 			}
 		}
 	}
 
 	bool OnUserUpdate(float fElapsedTime) {
-		drawTileMap(map, 3, 3, 0, 0, 5, 5 * aspectRatio, false);
-		
+		for (int i = 0; i < chunks.size(); i++) {
+			drawTileMap(chunks[i]->data, chunks[i]->width, chunks[i]->height, 60-i*chunks[i]->width, 120, 120, 120 * aspectRatio, false);
+		}
+		/*
 		n++;
 		if (n >= 400 * 300) {
 			stop = true;
@@ -64,7 +88,7 @@ public:
 			}
 			Draw(n % 400, n / 400, olc::Pixel(255, 255, 255));
 		}
-
+		*/
 		return true;
 	}
 
@@ -72,6 +96,10 @@ public:
 		for (int i = 0; i < tiles.size(); i++) {
 			delete tiles[i];
 		}
+		for (int i = 0; i < chunks.size(); i++) {
+			delete chunks[i];
+		}
+		open_simplex_noise_free(oSimpContext);
 		return true;
 	}
 
